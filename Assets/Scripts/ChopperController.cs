513ca38d4d2d9771bb6ff663eae367bcdeea3ad4
rleinfellner <richard@modulo17.com>
2017-02-18 11:01:02 +0000

    Added Chopper controls

---
+++ Assets/Scripts/ChopperController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChopperController : MonoBehaviour {
+
+
+	Animator	mANI;
+	Rigidbody	mRB;
+
+	RotorProxy	mRP;		//Rotor Helper
+
+	// Grab references to stuff we will need
+	void Start () {
+		mANI = GetComponent<Animator> ();
+		mRB = GetComponent<Rigidbody> ();
+		mRP = GetComponentInChildren<RotorProxy> (); //Get Reference to rotor helper
+		mRB.centerOfMass=Vector3.down * 0.5f;		//Makes it more stable
+	}
+
+
+	void FixedUpdate () {
+		float	tRotorSpeed = mRP.RotorSpeed/5.0f;
+		float	tThrust = InputController.GetInput (InputController.Directions.Fire);
+		float	tYaw = InputController.GetInput (InputController.Directions.MoveX);
+		float	tPitch = InputController.GetInput (InputController.Directions.MoveY);
+		mANI.SetBool ("EngineOn",(tThrust > 0f));
+		//mRB.MoveRotation(Quaternion.Euler(tYaw*15f,0,tPitch*15f));
+		Vector3	tForce = Vector3.zero;
+		Debug.Log (tRotorSpeed);
+		tForce.y = -Physics.gravity.y * 1.2f  * tRotorSpeed;
+		if (transform.position.y > 10f) {
+			Vector3	tVelocity = mRB.velocity;	//Clamp Height
+			tVelocity.y=0f;
+			mRB.velocity = tVelocity;
+		}
+		tForce.x = -tPitch * tRotorSpeed;
+		tForce.z = tYaw	* tRotorSpeed;
+		Debug.Log (tForce);
+		mRB.AddForce (tForce);		//Engine 1.5x G and depends on Rotor speed
+	}
+
+	void	LateUpdate () {
+	}
+}
---
+++ Assets/Scripts/RotorProxy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotorProxy : MonoBehaviour {
+
+	public	float	RotorSpeed;
+
+	Animator	mANI;
+
+	// Use this for initialization
+	void Start () {
+		mANI = GetComponent<Animator> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mANI.SetFloat ("RotorSpeed", RotorSpeed);
+	}
+}
