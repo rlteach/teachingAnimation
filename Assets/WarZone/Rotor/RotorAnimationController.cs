05388e3b958a2739ef42d85b189387a612740645
rleinfellner <richard@modulo17.com>
2017-02-22 11:07:31 +0000

    Added Code rotor

--- Assets/WarZone/Rotor/RotorAnimationController.cs
+++ Assets/WarZone/Rotor/RotorAnimationController.cs
@@ -2,28 +2,30 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class AnimationTest : MonoBehaviour {
+public class RotorAnimationController : MonoBehaviour {
+
 
 	public	Text	DebugText;
 
-	public	float			RotorForce;
-	public	float			RotorMaxSpeed;
-	public	GameObject		RotorGO;
+	public	float			RotorForce;			//Force applied to Rotor
 
-	float	RotorSpeed;
+	public	float			RotorMaxSpeed;		//Current Rotor Speed
+
+	float	RotorSpeed;			//Current Speed
 
 	Animator	mANI;
 
 	// Use this for initialization
 	void Start () {
-		mANI = GetComponent<Animator> ();	
+		mANI = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		mANI.SetBool ("Running", (InputController.GetInput (InputController.Directions.Thrust) > 0f));
 		RotorSpeed = Mathf.Clamp (RotorSpeed + RotorForce, 0f, RotorMaxSpeed);
 		mANI.SetFloat ("RotorSpeed", RotorSpeed);
-		RotorGO.transform.Rotate(0, RotorSpeed * Time.deltaTime, 0);
+		transform.Rotate(0, RotorSpeed * Time.deltaTime, 0);
 		DebugText.text=string.Format("Force {0:f2} Speed {1:f2}",RotorForce,RotorSpeed);
 	}
 }
