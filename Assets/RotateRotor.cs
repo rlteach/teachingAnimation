9666e81aa43136daa239ae091c6aec22038963f5
rleinfellner <richard@modulo17.com>
2017-02-22 13:04:41 +0000

    Added behavior

---
+++ Assets/RotateRotor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateRotor : StateMachineBehaviour {
+
+
+    public float RotorMaxSpeed;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        DoRotor(animator);
+    }
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        DoRotor(animator);
+    }
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        DoRotor(animator);
+    }
+    void DoRotor(Animator vAni) {
+        float tRotorSpeed = vAni.GetFloat("RotorSpeed");
+        float tRotorForce =vAni.GetFloat("RotorForce");
+        tRotorSpeed = Mathf.Clamp(tRotorSpeed + tRotorForce, 0f, RotorMaxSpeed);
+        vAni.SetFloat("RotorSpeed", tRotorSpeed);
+        vAni.gameObject.transform.Rotate(0, tRotorSpeed * Time.deltaTime, 0);
+        Debug.Log(tRotorSpeed);
+    }
+
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
--- Assets/WarZone/Rotor/RotorAnimationController.cs
+++ Assets/WarZone/Rotor/RotorAnimationController.cs
@@ -7,12 +7,6 @@ public class RotorAnimationController : MonoBehaviour {
 
 	public	Text	DebugText;
 
-	public	float			RotorForce;			//Force applied to Rotor
-
-	public	float			RotorMaxSpeed;		//Current Rotor Speed
-
-	float	RotorSpeed;			//Current Speed
-
 	Animator	mANI;
 
 	// Use this for initialization
@@ -22,10 +16,6 @@ public class RotorAnimationController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		mANI.SetBool ("Running", (InputController.GetInput (InputController.Directions.Thrust) > 0f));
-		RotorSpeed = Mathf.Clamp (RotorSpeed + RotorForce, 0f, RotorMaxSpeed);
-		mANI.SetFloat ("RotorSpeed", RotorSpeed);
-		transform.Rotate(0, RotorSpeed * Time.deltaTime, 0);
-		DebugText.text=string.Format("Force {0:f2} Speed {1:f2}",RotorForce,RotorSpeed);
+		mANI.SetBool ("Running", (InputController.GetInput (InputController.Directions.Thrust) > 0f));      //Set flag to tell animator to start rotor
 	}
 }
